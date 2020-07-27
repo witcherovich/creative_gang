78c3e4721b5b1d7f27c2aa4509a110dd0fe0c83f
Andrii Yudov <a.yudov@spd-ukraine.com>
2020-07-27 09:59:37 +0300

    Remove useless code from project

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './app.css';
+
+export default class App extends React.Component {
+    render() {
+        return <div className="app">Our <b>project</b> will be here</div>
+    }
+}
